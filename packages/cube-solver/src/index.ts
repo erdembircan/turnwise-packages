efce7e10ecf36421f9766ac1806c7082bca97a6d
Erdem Bircan <erdem.erdembircan@gmail.com>
2026-09-17 12:01:13 +0300

    [feat]: add opaque Cube with cubeFromMoves, applyMoves, isSolved

--- packages/cube-solver/src/index.ts
+++ packages/cube-solver/src/index.ts
@@ -1,3 +1,5 @@
+export { applyMoves, cubeFromMoves, isSolved } from './cube';
+export type { Cube } from './cube';
 export { Faces } from './face';
 export type { Face } from './face';
 export { Moves, faceOf, formatAlgorithm, inverse, turnsOf } from './move';
